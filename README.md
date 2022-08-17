415b6fea0d98ea7a89df4b1cc9d2561c86853a5b
Carmen Popoviciu <cpopoviciu@cloudflare.com>
2022-08-17 21:18:51 +0200

    feat: Implement classification model client-side
    This commit scaffolds the basic application we will use across our
    experiments. The UI consists of an input field through which users
    can enter the text that will be analyzed by the TF Toxicity model.
    The result of the model's classification, will be displayed back
    to the user via the same UI.
    
    In this commit, both the model loading and the classification happen
    on the client side. The model is loaded once the page is loaded, for
    better performance. While performing the input text sentiment
    analysis, we internally keep track of two metrics:
    - the elapsed time from when the input text was submitted for
    analysis, to when the result is returned by the classifier
    - the average of the above value across the number of user text
    submissions
    
    The above metrics will remain consistent across all our experiments,
    in order to correctly evaluate which implementation has better
    performance.

---
+++ README.md
@@ -0,0 +1,31 @@
+# Message in a bottle, yeah
+Based on https://github.com/tensorflow/tfjs-models/tree/master/toxicity/demo
+
+## Description
+
+This demo shows how to use predictions produced by the Tensorflow Toxicity classifier.
+
+It is a basic attempt to showcase how ML models could be run on [Cloudflare Workers](https://workers.cloudflare.com/). Please check the reporsitory [tags](https://github.com/CarmenPopoviciu/message-in-a-bottle/tags) to view the progression of attempted implementations and solutions.
+
+## Requirements
+
+To run this project locally, make sure you have [Wrangler](https://developers.cloudflare.com/workers/wrangler/) installed.
+
+## Setup
+
+Install dependencies:
+
+```sh
+yarn
+```
+
+Launch a dev server and code away:
+
+```sh
+yarn dev
+```
+
+Build assets:
+```sh
+yarn build
+```
---
+++ src/index.ts
@@ -0,0 +1,106 @@
+/**
+ * @license
+ * Copyright 2019 Google LLC. All Rights Reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * =============================================================================
+ */
+
+import * as toxicity from '@tensorflow-models/toxicity';
+
+enum MESSAGES {
+  YAY = "Positive vibes received ✨🌟✨! Passing forward to the rest of the world 💛",
+  NAY = "How about sending a more positive vibe to the world? 🧡",
+}
+
+const inputEl = document.getElementById("classify-input") as HTMLInputElement;
+const classifyBtn = document.getElementById("classify-btn") as HTMLButtonElement;
+const messageEl = document.getElementsByClassName("message")[0] as HTMLDivElement;
+const elapsedTimeEl = document.getElementsByClassName("crr-elapsed-time")[0] as HTMLDivElement;
+const averageTimeEl = document.getElementsByClassName("average-elapsed-time")[0] as HTMLDivElement;
+
+let model;
+let labels: string[];
+let totalClassificationExecutionTime = 0;
+let iterations = 0;
+
+// Load TF model immediately so it is already available when needed 
+loadModel();
+
+classifyBtn.addEventListener("click", analyzeMessage);
+inputEl.addEventListener("keyup", (ev: KeyboardEvent) => {
+  if(ev.key === 'Enter') {
+    analyzeMessage();
+  }
+});
+
+async function analyzeMessage() {
+  const text = inputEl?.value;
+
+  /** 
+   * Measure how long it takes to get a prediction back from the 
+   * classifier. This will allow us to compare the performance of 
+   * different implementations
+   */
+   const start = performance.now();
+   const results = await classify([text]);
+   const end = performance.now();
+   /** */
+   
+  let containsToxicSentiments = false;
+
+  for(let i=0; i< results.length; i++) {
+    let classification = results[i];
+
+    for(let j=0; j<labels.length; j++) {
+      let label = labels[j];
+
+      // if it's not strictly false then it's toxic
+      if(classification[label] !== false) {
+        containsToxicSentiments = true;
+        break;
+      }
+    }
+  }
+
+  const message = containsToxicSentiments ? MESSAGES.NAY : MESSAGES.YAY;
+  messageEl.textContent = message;
+
+  const crrElapsedTime = end - start;
+  elapsedTimeEl.textContent = `Classification elapsed time: ${crrElapsedTime}ms`;
+
+  totalClassificationExecutionTime += crrElapsedTime;
+  iterations += 1;
+  averageTimeEl.textContent = `Avg. classification time: ${totalClassificationExecutionTime / iterations}ms`;
+}
+
+/**
+ * Predicts whether the given input is toxic or not
+ */
+async function classify(inputs: string[]) {
+  const results = await model.classify(inputs);
+  return inputs.map((d, i) => {
+    const obj = { text: d };
+    results.forEach((classification) => {
+      obj[classification.label] = classification.results[i].match;
+    });
+    return obj;
+  });
+};
+
+/**
+ * loads Tensorflow model
+ */
+async function loadModel(): Promise<void> {
+  model = await toxicity.load();
+  labels = model.model.outputNodes.map((d) => d.split('/')[0]);
+};
